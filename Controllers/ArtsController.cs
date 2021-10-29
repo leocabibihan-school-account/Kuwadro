5cb9bd4ff195ddf0b30541f31d4a9d96786fdae2
Leo Cabibihan <leocabibihan@outlook.com>
2021-10-29 10:47:01 +0300

    basic crud

---
+++ Controllers/ArtsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Kuwaderno.Data;
+using Kuwaderno.Models;
+
+namespace Kuwaderno.Controllers
+{
+    public class ArtsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Arts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.artList.ToListAsync());
+        }
+
+        // GET: Arts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var art = await _context.artList
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (art == null)
+            {
+                return NotFound();
+            }
+
+            return View(art);
+        }
+
+        // GET: Arts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Arts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Image,Title,Description,CreationDate,UserId")] Art art)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(art);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(art);
+        }
+
+        // GET: Arts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var art = await _context.artList.FindAsync(id);
+            if (art == null)
+            {
+                return NotFound();
+            }
+            return View(art);
+        }
+
+        // POST: Arts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Image,Title,Description,CreationDate,UserId")] Art art)
+        {
+            if (id != art.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(art);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtExists(art.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(art);
+        }
+
+        // GET: Arts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var art = await _context.artList
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (art == null)
+            {
+                return NotFound();
+            }
+
+            return View(art);
+        }
+
+        // POST: Arts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var art = await _context.artList.FindAsync(id);
+            _context.artList.Remove(art);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtExists(int id)
+        {
+            return _context.artList.Any(e => e.Id == id);
+        }
+    }
+}
